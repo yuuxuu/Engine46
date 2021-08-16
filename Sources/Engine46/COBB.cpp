3c064ca1a76d9728e402c4e8a7a8a8b98154892b
Yuu <kimura.yuu1120@gmail.com>
2021-08-16 23:08:40 +0900

    OBBクラスを追加

---
+++ Sources/Engine46/COBB.cpp
@@ -0,0 +1,27 @@
+﻿/**
+ * @file COBB.cpp
+ * @brief
+ * @author 木村優
+ * @date 2021/08/10
+ */
+
+#include "COBB.h"
+#include "CActor.h"
+
+namespace Engine46 {
+
+	// コンストラクタ
+	COBB::COBB()
+	{}
+
+	// デストラクタ
+	COBB::~COBB()
+	{}
+
+	// 初期化
+	void COBB::Initialize() {
+
+		
+	}
+
+} // namespace
---
+++ Sources/Engine46/COBB.h
@@ -0,0 +1,28 @@
+﻿/**
+ * @file COBB.h
+ * @brief
+ * @author 木村優
+ * @date 2021/08/10
+ */
+
+#pragma once
+
+namespace Engine46 {
+
+	// 前方宣言
+	class CActorBase;
+	class CMeshBase;
+
+	class COBB {
+		
+	public:
+		COBB();
+		~COBB();
+
+		void Initialize();
+
+	};
+
+} // namespace
+
+
